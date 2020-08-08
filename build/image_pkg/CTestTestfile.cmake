9253866bb0ca8e22c02a569b48f841090c736a53
DouBle0099 <zrj1045980916@gmail.com>
2020-08-08 16:14:45 +0800

    modification test

--- build/image_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zrj/Wattman_Code/src/image_pkg
-# Build directory: /home/zrj/Wattman_Code/build/image_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
